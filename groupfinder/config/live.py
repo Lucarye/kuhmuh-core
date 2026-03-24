5f23de70b430092a4fae14cf54f6625b42af337f
Lucarye <45335928+Lucarye@users.noreply.github.com>
2026-03-24 23:57:36 +0100

    update live context configuration
    PATCH: config/live.py

--- groupfinder/config/live.py
+++ groupfinder/config/live.py
@@ -6,12 +6,22 @@ from .shared import LIVE_CONTEXT_KEY, LIVE_STORAGE_NAMESPACE
 
 LIVE_CONTEXT = Context(
     context_key=LIVE_CONTEXT_KEY,
+
+    # Kuhmuh Discord
     guild_id=1198649628787212458,
+
     command_channel_ids=(
+        # LIVE: Gruppensuche V2
         1486128654332203140,
     ),
+    command_category_ids=(),
+
+    # Noch nicht gesetzt
     dashboard_channel_id=0,
     log_channel_id=0,
+
+    # LIVE aktuell ohne Rollenbeschränkung
     allowed_role_ids=(),
+
     storage_namespace=LIVE_STORAGE_NAMESPACE,
 )
